3dce5fd151f7b4ae131965e458c764f6e6335728
zaxx1 <hafidzzakky321@gmail.com>
2024-10-25 15:05:27 +0700

    Add files via upload

---
+++ src/logo.js
@@ -0,0 +1,14 @@
+function printLogo(){
+    console.log(`
+    ██████╗ ██╗   ██╗████████╗██╗ ██████╗ ██████╗  ██████╗ ██╗      
+    ██╔══██╗██║   ██║╚══██╔══╝██║██╔════╝██╔═══██╗██╔═══██╗██║      
+    ██████╔╝██║   ██║   ██║   ██║██║     ██║   ██║██║   ██║██║      
+    ██╔═══╝ ██║   ██║   ██║   ██║██║     ██║   ██║██║   ██║██║      
+    ██║     ╚██████╔╝   ██║   ██║╚██████╗╚██████╔╝╚██████╔╝███████╗ 
+    ╚═╝      ╚═════╝    ╚═╝   ╚═╝ ╚═════╝ ╚═════╝  ╚═════╝ ╚══════╝ 
+            `.cyan);
+    console.log('[+] ❤️ Việt hóa và chia sẻ tool miễn phí bởi [https://t.me/toolcodecheat] ❤️ '.green);
+    console.log('[+] Error? PM Telegram [https://t.me/NothingYub]'.red);
+    console.log('[+] 📣 New Feature Update 📣  [https://t.me/puticoolbot]'.blue);
+}
+module.exports = printLogo;
